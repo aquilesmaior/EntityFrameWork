95b94f58a63e61448f89b6fe7b1e9a405d7530be
Aquiles Maior <aquilesmaior@gmail.com>
2021-03-19 08:48:45 -0300

    Adicionar arquivos de projeto.

---
+++ EFCore.WebAPI/Models/Arma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Models {
+    public class Arma {
+        // Arma é uma relação de 1 pra 1
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Heroi Heroi { get; set; }
+        public int HeroiId { get; set; }
+    }
+}
